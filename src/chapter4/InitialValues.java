9d914b64efa6f61e705be117ceacb52e07a23fff
noprom <tyee.noprom@qq.com>
2015-06-19 08:25:31 +0800

    InitialValues.java

--- src/chapter4/InitialValues.java
+++ src/chapter4/InitialValues.java
@@ -30,8 +30,9 @@ public class InitialValues {
   public static void main(String[] args) {
     Measurement d = new Measurement();
     d.print();
-    /* In this case you could also say:
-    new Measurement().print();
-    */
+    /**
+     *  In this case you could also say:
+     *  new Measurement().print();
+     */
   }
 } ///:~
